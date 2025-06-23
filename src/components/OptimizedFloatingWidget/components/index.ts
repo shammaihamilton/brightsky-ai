867e5721129833f42b85d86ce8fb2cb5666ae380
shammai hamilton <shammaihamilton@gmail.com>
2025-06-24 02:56:54 +0300

    fix: update import paths in components/index.ts to match actual styled component file names

--- src/components/OptimizedFloatingWidget/components/index.ts
+++ src/components/OptimizedFloatingWidget/components/index.ts
@@ -1,11 +1,11 @@
 // Export all optimized components
 export { default as OptimizedFloatingWidget } from '../index';
-export { default as FloatingButton } from './FloatingButton';
-export { default as ChatPanel } from './ChatPanel';
-export { default as DropdownMenu } from './DropdownMenu';
-export { default as MessageList } from './MessageList';
-export { default as MessageItem } from './MessageItem';
-export { default as ChatInput } from './ChatInput';
+export { default as FloatingButton } from './FloatingButtonStyled';
+export { default as ChatPanel } from './ChatPanelStyled';
+export { default as DropdownMenu } from './DropdownMenuStyled';
+export { default as MessageList } from './MessageListStyled';
+export { default as MessageItem } from './MessageItemStyled';
+export { default as ChatInput } from './ChatInputStyled';
 
 // Export hooks
 export { useInjectCSS } from '../hooks/useInjectCSS';
